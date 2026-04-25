601bf34ece0450d153683bea9fac4ca26eae15f5
anacg096 <anacilleguti@gmail.com>
2026-04-25 21:42:17 +0200

    se añaden las vistas a nuestro proyecto

---
+++ src/main/java/com/portfolio/my_portfolio_backend/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.portfolio.my_portfolio_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.portfolio.my_portfolio_backend.service.IEducationService;
+import com.portfolio.my_portfolio_backend.service.IExperienceService;
+import com.portfolio.my_portfolio_backend.service.IPersonalInfoService;
+import com.portfolio.my_portfolio_backend.service.ISkillService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+    private final IPersonalInfoService personalInfoService;
+    private final IEducationService educationService;
+    private final ISkillService skillService;
+    private final IExperienceService experienceService;
+
+    @GetMapping("/")
+    public String showIndex(Model model) {
+        System.out.println("Mostrando la página de inicio");
+
+        model.addAttribute("personalInfo", personalInfoService.findAll().getFirst());
+        model.addAttribute("experienceList", experienceService.findAll());
+        model.addAttribute("educationList", educationService.findAll());
+        model.addAttribute("skills", skillService.findAll());
+
+        return "index";
+    }
+}
